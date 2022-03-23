9e11816fd56bce01b04490470b8087961a264c34
Mikhail Zaytsev <m.zaytsev.23@gmail.com>
2022-03-23 17:49:22 +0300

    simplify colors assignment

--- src/Colors.h
+++ src/Colors.h
@@ -1,12 +1,12 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 
-const sf::Color grayColor = sf::Color::Color(150, 150, 150, 150);
-const sf::Color txtColor = sf::Color::Color(210, 210, 210);
-const sf::Color redColor = sf::Color::Color(128, 0, 0);
-const sf::Color blueColor = sf::Color::Color(11, 97, 140);
-const sf::Color blackColor = sf::Color::Color(30, 30, 30);
-const sf::Color nameColor = sf::Color::Color(255, 219, 163);
-const sf::Color yellowColor = sf::Color::Color(220, 220, 170);
-const sf::Color greenColor = sf::Color::Color(40, 140, 40);
-const sf::Color whiteColor = sf::Color::Color(255, 255, 255);
+const sf::Color grayColor(150, 150, 150, 150);
+const sf::Color txtColor(210, 210, 210);
+const sf::Color redColor(128, 0, 0);
+const sf::Color blueColor(11, 97, 140);
+const sf::Color blackColor(30, 30, 30);
+const sf::Color nameColor(255, 219, 163);
+const sf::Color yellowColor(220, 220, 170);
+const sf::Color greenColor(40, 140, 40);
+const sf::Color whiteColor(255, 255, 255);
